620bef0f318acb4320034915153466ecbb11727f
soeyeon <wjsthdus8941@gmail.com>
2021-12-09 17:55:58 +0900

    first commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //database : 'mongodb://localhost:27017/meanauth',
+  database : "mongodb+srv://new_user_1:aaa@cluster0.o8dnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secret : 'asfsafsdadddd'
+}
